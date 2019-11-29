3b55cc3a5c87cb8b2f83917e809352776927a224
Elzhas <esuleymenov@at-consulting.ru>
2019-11-29 14:46:14 +0600

    last improvments

--- app/src/main/java/kz/atc/mobapp/presenters/SendSMSPagePresenter.kt
+++ app/src/main/java/kz/atc/mobapp/presenters/SendSMSPagePresenter.kt
@@ -23,43 +23,55 @@ class SendSMSPagePresenter(val ctx: Context) :
     override fun bindIntents() {
         val sendSMSScreenState: Observable<SendSMSPageState> =
             intent(SendSMSScreenView::sendSMSButtonIntent).flatMap { auth ->
-                UserInteractor().userService.userTypeCheck(auth).flatMap { result ->
-                    if (result.name != "Пользователь мобильной связи") {
-                        Observable.just(SendSMSPageState.ErrorState("Вы не являетесь пользователем мобильной связи"))
-                    } else {
-                        UserInteractor().userService.sendSMS(RequestBody.create(MediaType.parse("text/plain"), auth)).flatMap{
-                            Observable.just(SendSMSPageState.SmsSend)
-                        }
-                    }
-                }
-                    .onErrorResumeNext { error: Throwable ->
-                    var errMessage = error.localizedMessage
-                    if (error is HttpException) {
-                        if (error is HttpException) {
-                            errMessage = if (error.code() == 409) {
-                                "Вы не являетесь пользователем мобильной связи"
-                            } else {
-                                val errorBody = error.response()!!.errorBody()
-
-                                val adapter =
-                                    gson.getAdapter<ErrorJson>(ErrorJson::class.java!!)
-                                Log.d("JSON", errorBody.toString())
-                                val errorObj = adapter.fromJson(errorBody!!.string())
-                                errorObj.error_description
+                if (auth.isBlank()) {
+                    Observable.just(SendSMSPageState.ErrorState("Введите номер телефона"))
+                } else {
+                    UserInteractor().userService.userTypeCheck(auth).flatMap { result ->
+                        if (result.name != "Пользователь мобильной связи") {
+                            Observable.just(SendSMSPageState.ErrorState("Вы не являетесь пользователем мобильной связи"))
+                        } else {
+                            UserInteractor().userService.sendSMS(
+                                RequestBody.create(
+                                    MediaType.parse(
+                                        "text/plain"
+                                    ), auth
+                                )
+                            ).flatMap {
+                                Observable.just(SendSMSPageState.SmsSend)
                             }
                         }
                     }
-                    Observable.just(SendSMSPageState.ErrorState(errMessage))
-                }.subscribeOn(Schedulers.io())
+                        .onErrorResumeNext { error: Throwable ->
+                            var errMessage = error.localizedMessage
+                            if (error is HttpException) {
+                                if (error is HttpException) {
+                                    errMessage = if (error.code() == 409) {
+                                        "Вы не являетесь пользователем мобильной связи"
+                                    } else {
+                                        val errorBody = error.response()!!.errorBody()
+
+                                        val adapter =
+                                            gson.getAdapter<ErrorJson>(ErrorJson::class.java!!)
+                                        Log.d("JSON", errorBody.toString())
+                                        val errorObj = adapter.fromJson(errorBody!!.string())
+                                        errorObj.error_description
+                                    }
+                                }
+                            }
+                            Observable.just(SendSMSPageState.ErrorState(errMessage))
+                        }.subscribeOn(Schedulers.io())
+                }
             }
 
-        val defaultState: Observable<SendSMSPageState> = intent(SendSMSScreenView::defaultIntent).map<SendSMSPageState> {
-            SendSMSPageState.DefaultState
-        }
+        val defaultState: Observable<SendSMSPageState> =
+            intent(SendSMSScreenView::defaultIntent).map<SendSMSPageState> {
+                SendSMSPageState.DefaultState
+            }
 
-        val allIntents = Observable.merge(sendSMSScreenState, defaultState).observeOn(AndroidSchedulers.mainThread())
+        val allIntents = Observable.merge(sendSMSScreenState, defaultState)
+            .observeOn(AndroidSchedulers.mainThread())
 
 
-        subscribeViewState(allIntents, SendSMSScreenView :: render)
+        subscribeViewState(allIntents, SendSMSScreenView::render)
     }
 }
