7d4a02e7d1f3ac5444ff47f1525fc71b3fdef0ba
Elzhas <esuleymenov@at-consulting.ru>
2019-12-24 19:04:32 +0600

    Started Services page

---
+++ app/src/main/java/kz/atc/mobapp/fragments/main/ServicesFragment.kt
@@ -0,0 +1,26 @@
+package kz.atc.mobapp.fragments.main
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kz.atc.mobapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ServicesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_services, container, false)
+    }
+
+
+}
